cba00a137bdf570877e63e3c7ced8e103ce1e918
Pegoku <pegoku08@gmail.com>
2024-06-28 22:56:00 +0200

    add buttons

---
+++ game.js
@@ -0,0 +1,12 @@
+// 1 Rock     Scissors Lizard
+// 2 Paper    Spock Rock
+// 3 Scissors Lizard Paper
+// 4 Lizard   Spock Paper
+// 5 Spock    Scissors Rock
+function ini() {
+    document.writeln(
+      '<div class="buttons" style="margin: auto; width: 50%; padding: 10px;"><button onclick="rock()"><img src="https://statics.memondo.com/p/s1/crs/2023/12/CR_1300692_dedd199497d34d7fb273663f5744e7d6_the_rock.jpg?cb=5885355" alt="" style="display: block; margin-left: margin-right: auto; max-width:150px; max-height:150px;"></button><button onclick="paper()"><img src="https://kleaning.es/935-large_default/8-packs-de-12-rollos-papel-de-vater.jpg" alt="" style="display: block; margin-left: margin-right: auto; max-width:150px; max-height:150px;"></button><button onclick="scissors()"><img src="https://www.partyfiesta.com/16906-large_default/tijeras-trolls.jpg" alt="" style="display: block; margin-left: margin-right: auto; max-width:150px; max-height:150px;"></button><button onclick="lizard()"><img src="https://www.google.com/url?q=https://encrypted-tbn0.gstatic.com/images?q%3Dtbn:ANd9GcTMcdHxb8JKIDNHhnd8cliLYywIXuv2gc42XJWzbkncLZHaR05NFDQV9Wm3cnzEN5EeAk8%26usqp%3DCAU&source=gmail&ust=1715754914871000&usg=AOvVaw1shtBt6_5u4RrPNEi4G-EY" alt="" style="display: block; margin-left: margin-right: auto; max-width:150px; max-height:150px;"></button><button onclick="spock()"><img src="https://www.lanacion.com.ar/resizer/v2/su-personaje-en-star-trek-lo-llevo-a-lo-mas-alto-TVCU6GCJZZCJ5KY7DB7EVZBY7Y.jpg?auth=6ce3566c686a4af60155f8f69787b79f59744982e174b972f355deedb477148d&width=420&height=280&quality=70&smart=true" alt="" style="display: block; margin-left: margin-right: auto; max-width:150px; max-height:150px;"></button></div>'
+    );
+    document.body.style.backgroundColor = "#99ff99";
+  }
+  ini();
